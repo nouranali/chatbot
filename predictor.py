b4e906f8258df7bf853df4de24b9190a8ccf77b1
Nouran Ali <nouranalimohammed@gmail.com>
2021-03-02 00:26:20 +0200

    test

---
+++ predictor.py
@@ -0,0 +1,14 @@
+from allennlp.predictors.predictor import Predictor as AllenNLPPredictor
+
+
+class Predictor:
+    def __init__(self, config):
+        self.predictor = AllenNLPPredictor.from_path(
+            "https://storage.googleapis.com/allennlp-public-models/bidaf-elmo-model-2018.11.30-charpad.tar.gz"
+        )
+
+    def predict(self, payload):
+        prediction = self.predictor.predict(
+            passage=payload["passage"], question=payload["question"]
+        )
+        return prediction["best_span_str"]
